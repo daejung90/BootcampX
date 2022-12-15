f1c6b28932602f4375af2a4598185143112c984f
daejung90 <djung.ca@gmail.com>
2022-12-15 09:26:44 -0500

    connecting to database using javascript and writing queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,34 @@
+const { Pool } = require('pg');
+const process = require ('process');
+
+
+const pool = new Pool({
+  user: 'd.jung',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+console.log('Connected to database!')
+
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = 'JUL02'
+// ORDER BY teacher;
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
