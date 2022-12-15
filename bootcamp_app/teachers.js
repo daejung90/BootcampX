853ecaaae5193db05bbbc036065a1b89d3b70f0a
daejung90 <djung.ca@gmail.com>
2022-12-15 09:57:34 -0500

    adding parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -18,15 +18,18 @@ console.log('Connected to database!')
 // WHERE cohorts.name = 'JUL02'
 // ORDER BY teacher;
 
-pool.query(`
+const values = [process.argv[2] || 'JUL02'];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
